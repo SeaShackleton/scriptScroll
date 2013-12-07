332cbb44e3a172ba190f17888270fd54ba999a0f
piobuilleann <cas7374@gmail.com>
2013-12-06 21:44:42 -0600

    wrapped virtical scrolling area

--- scriptScroll.js
+++ scriptScroll.js
@@ -21,33 +21,62 @@
 			$e.handleTop = function(top){
 				handle.css("top",top+"px");						
 			};
+			$e.handleLeft = function(top){
+				handle.css("left",top+"px");						
+			};
 			$e.onScroll = function(e){
 				var elementHeight = $e.height(), scrollingHeight = scrolling.innerHeight();
 				//FIREFOX
 				if(e.originalEvent.detail){
-					if(e.originalEvent.detail > 0) {			
-						$e.scrollTop($e.scrollTop()+options.scrollSpeed);
-						interactedToTop = (($e.scrollTop() / (scrollingHeight- elementHeight) )*(track.innerHeight() - handle.innerHeight() ));
-						$e.handleTop(interactedToTop);
+					if(e.originalEvent.detail > 0) {
+						if(options.scrollVertical === false){
+							//horizontal
+							scrollingContainer.scrollLeft(scrollingContainer.scrollLeft()+options.scrollSpeed);
+							$e.handleLeft((scrollingContainer.scrollLeft() / (scrolling.get(0).scrollWidth- $e.width()) )*(track.innerWidth() - handle.innerWidth() ));						
+						}else{
+							//vertical
+							scrollingContainer.scrollTop(scrollingContainer.scrollTop()+options.scrollSpeed);
+							$e.handleTop((scrollingContainer.scrollTop() / (scrolling.get(0).scrollHeight- $e.height()) )*(track.innerHeight() - handle.innerHeight() ));
+						}
 					}else {
-						$e.scrollTop($e.scrollTop()-options.scrollSpeed);
-						interactedToTop = (($e.scrollTop() / (scrollingHeight- elementHeight) )*(track.innerHeight() - handle.innerHeight() ));				
-						$e.handleTop(interactedToTop);
+						if(options.scrollVertical === false){
+							//horizontal
+							scrollingContainer.scrollLeft(scrollingContainer.scrollLeft()-options.scrollSpeed);
+							$e.handleLeft((scrollingContainer.scrollLeft() / (scrolling.get(0).scrollWidth- $e.width()) )*(track.innerWidth() - handle.innerWidth() ));								
+						}else{
+							//vertical
+							scrollingContainer.scrollTop(scrollingContainer.scrollTop()-options.scrollSpeed);
+							$e.handleTop((scrollingContainer.scrollTop() / (scrolling.get(0).scrollHeight- $e.height()) )*(track.innerHeight() - handle.innerHeight() ));						
+						}
 					}				
 				}
 				//IE OPERA SAFARI
 				if(e.originalEvent.wheelDelta){
-					if(e.originalEvent.wheelDelta < 0) {			
-						$e.scrollTop($e.scrollTop()+options.scrollSpeed);
-						interactedToTop = (($e.scrollTop() / (scrollingHeight- elementHeight) )*(track.innerHeight() - handle.innerHeight() ));
-						$e.handleTop(interactedToTop);
+					if(e.originalEvent.wheelDelta < 0) {
+						if(options.scrollVertical === false){
+							//horizontal
+							scrollingContainer.scrollLeft(scrollingContainer.scrollLeft()+options.scrollSpeed);
+							$e.handleLeft((scrollingContainer.scrollLeft() / (scrolling.get(0).scrollWidth- $e.width()) )*(track.innerWidth() - handle.innerWidth() ));	
+							
+						}else{
+							//vertical
+							scrollingContainer.scrollTop(scrollingContainer.scrollTop()+options.scrollSpeed);
+							$e.handleTop((scrollingContainer.scrollTop() / (scrolling.get(0).scrollHeight- $e.height()) )*(track.innerHeight() - handle.innerHeight() ));
+						}
 					}else {
-						$e.scrollTop($e.scrollTop()-options.scrollSpeed);
-						interactedToTop = (($e.scrollTop() / (scrollingHeight- elementHeight) )*(track.innerHeight() - handle.innerHeight() ));				
-						$e.handleTop(interactedToTop);
+						if(options.scrollVertical === false){
+							//horizontal
+							scrollingContainer.scrollLeft(scrollingContainer.scrollLeft()-options.scrollSpeed);
+							$e.handleLeft((scrollingContainer.scrollLeft() / (scrolling.get(0).scrollWidth- $e.width()) )*(track.innerWidth() - handle.innerWidth() ));							
+						}else{
+							//vertical
+							scrollingContainer.scrollTop(scrollingContainer.scrollTop()-options.scrollSpeed);
+							$e.handleTop((scrollingContainer.scrollTop() / (scrolling.get(0).scrollHeight- $e.height()) )*(track.innerHeight() - handle.innerHeight() ));						
+						}
 					}				
 				
 				}
+			
 				track.css("top", $e.scrollTop());
 				
 				return false;
@@ -62,37 +91,16 @@
 				
 				if(options.scrollVertical === false){
 					//HORIZONTAL SCROLL
-					
-					
-					
-					
-					
-					
-					
-					
 					//DEFINE THE SCROLLINT CONTAINER
 					$e.wrapInner( $('<div>',{'class':'scrolling'}).css({position:'relative', height:'120px'})  );
 					scrolling = $e.find('.scrolling');
-					
-					
-					
 					scrollingContainer = $('<div>',{'class':'scrollingContainer'}).css({backgroundColor:'pink', overflow:'hidden',  'white-space':'nowrap'})
 					$e.append(scrollingContainer);
 					scrollingContainer.append(scrolling)
-					
-					
-					console.log(scrolling.get(0).scrollWidth)
-
-					
-					
 					track = $("<div>",{'class':'scrollingTrack', css:{width:$e.width(), backgroundColor: options.trackColor, height:'10px', top:"0px", right:"0px", position:"relative"}});
-					//height:(($e.innerHeight() / scrolling.outerHeight()) * track.innerHeight())
 					handle = $("<div>",{'class':"scrollHandle", css:{height:'100%', width:(($e.innerWidth() / scrolling.get(0).scrollWidth) * track.innerWidth()), backgroundColor: options.handleColor, marginTop:'0px', cursor:'pointer'}});
 					track.append(handle);						
 					$e.append(track);
-					
-					
-					
 					if(options.trackOpposing === false){
 						$e.prepend(track);
 						scrollingContainer.css('margin-top',options.contentPadding)
@@ -100,7 +108,6 @@
 						scrollingContainer.css('margin-bottom',options.contentPadding)
 						$e.append(track);
 					}
-					
 					//IF THERE IS DATA AVAILABLE, ADD IT TO THE END OF THE CURRENT CONTENT WITHIN scrolling
 					if(options.data.length > 0){
 						scrolling.append(options.data);
@@ -109,49 +116,42 @@
 						axis: "x", 
 						containment: "parent",
 						drag: function(event, ui) {
-							//console.log(ui.position.left);
 							currentPos = (ui.position.left / (track.innerWidth()-handle.innerWidth())) *(scrolling.get(0).scrollWidth-$e.css("width").replace("px",""));
 							interactedToTop = currentPos;
 							scrollingContainer.scrollLeft(currentPos)		
 						} 
 					});		
-					
-					
-					
-					
-					
-
-
-
-
-
-
-
-
-					}else{
-					//VERTICAL SCROLL
-					//CREATE THE CONTAINER TO BE SCROLLED AND ADD THE APPROPRIATE CONTENT
-					$e.wrapInner( $('<div>',{'class':'scrolling'}).css({ width: ($e.width() - options.handleWidth - options.contentPadding), "margin-right": "20px", position:'relative'})  );
-					//DEFINE THE SCROLLINT CONTAINER
+				}else{
+					$e.wrapInner( $('<div>',{'class':'scrolling'}).css({ width: ($e.width() - options.handleWidth - options.contentPadding),  position:'relative'})  );
 					scrolling = $e.find('.scrolling');
+					scrolling.css('height',scrolling.get(0).scrollHeight)
+					scrollingContainer = $('<div>',{'class':'scrollingContainer'}).css({overflow:'hidden', height:'100%', width:($e.width()-options.handleWidth-options.contentPadding)})
+					$e.append(scrollingContainer);
+					scrollingContainer.append(scrolling)
+					track = $("<div>",{'class':'scrollingTrack', css:{height: $e.height(), backgroundColor: options.trackColor, width: options.handleWidth, top:"0px", right:"0px", position:"absolute"}});
+					handle = $("<div>",{'class':"scrollHandle", css:{width:'100%', height:(($e.innerHeight() / scrolling.get(0).scrollHeight) * track.innerHeight()), backgroundColor: options.handleColor, marginTop:'0px', cursor:'pointer'}});
+					track.append(handle);						
+					$e.append(track);
+					if(options.trackOpposing === false){
+						$e.prepend(track);
+						//scrollingContainer.css('margin-right',options.contentPadding)
+					}else{
+						scrollingContainer.css('margin-left',options.contentPadding)
+						$e.append(track);
+					}
 					//IF THERE IS DATA AVAILABLE, ADD IT TO THE END OF THE CURRENT CONTENT WITHIN scrolling
 					if(options.data.length > 0){
 						scrolling.append(options.data);
-					}
-					track = $("<div>",{'class':'scrollingTrack', css:{height: $e.height(), backgroundColor: options.trackColor, width: options.handleWidth, top:"0px", right:"0px", position:"absolute"}});
-					handle = $("<div>",{'class':"scrollHandle", css:{width:'100%', height:(($e.innerHeight() / scrolling.outerHeight()) * track.innerHeight()), backgroundColor: options.handleColor, marginTop:'0px', cursor:'pointer'}});
-					$e.append(track);
+					} 
 					handle.draggable({ 
 						axis: "y", 
 						containment: "parent",
 						drag: function(event, ui) {
-							currentPos = (ui.position.top / (track.innerHeight()-handle.innerHeight())) *(scrolling.innerHeight()-$e.css("height").replace("px",""));
+							currentPos = (ui.position.top / (track.innerHeight()-handle.innerHeight())) *(scrolling.get(0).scrollHeight-$e.height());
 							interactedToTop = currentPos;
-							$e.scrollTop(currentPos);
-							track.css("top", $e.scrollTop());				
+							scrollingContainer.scrollTop(currentPos)		
 						} 
-					});			
-					track.append(handle);				
+					});						
 				};				
 				handle.css({ 
 					'-moz-border-radius':options.handleCornerRadius, 
